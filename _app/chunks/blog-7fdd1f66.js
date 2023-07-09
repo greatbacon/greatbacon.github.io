1511cbc6579510886aaada314ddfb925046bb6ee
greatbacon <hamiltonjreed@gmail.com>
2023-07-09 15:40:42 -0600

    deploy 7/09/2023

---
+++ _app/chunks/blog-7fdd1f66.js
@@ -0,0 +1,1 @@
+const t=[{title:"The Coming Discourse Vacuum",subtitle:"Food for thought in a post-Twitter world",content:"There is no doubt that Twitter is collapsing. Millions of bytes have flown over the last few weeks describing the chaos both on the platform and in the private domain of its offices...",start:"2022-11-18",wrapped:null,link:"/blog/0"}];export{t as w};
